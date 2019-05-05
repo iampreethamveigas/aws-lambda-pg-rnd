459569ac1d39183a89c7f5df454fd98a3b892a0e
iampreethamveigas <veigasbros@gmail.com>
2019-05-06 01:38:46 +0530

    dbconfig_pg

--- index.js
+++ index.js
@@ -14,7 +14,84 @@ let dbConfig = {
 
 
 exports.handler = async (event, context, callback) => {
-    
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('Inital Response'),
+    };
+    const body_params = {}
+    const page = event['page']
+    const itemsPerPage = event['itemsPerPage']
+    let orderBy = event['orderBy'] || 'ASC'
+    orderBy = orderBy === 'ASC' ? orderBy : orderBy === 'DESC' ? orderBy : 'ASC'
+
+    const fieldName = event['fieldName'] || 'request_id'
+    body_params.page = page;
+    body_params.itemsPerPage = itemsPerPage;
+
+
+    try {
+        if (page && page != '') {
+            if (!Number.isInteger(page)) {
+                callback(new Error(`Page is not Interger type`));
+
+            }
+        }
+        if (itemsPerPage && itemsPerPage != '') {
+            if (!Number.isInteger(itemsPerPage)) {
+                callback(new Error(`itemsPerPage is not Interger type`));
+                return
+            }
+        }
+
+
+        // let offset_de = (body_params.page - 1) * body_params.itemsPerPage;
+        // let limit = body_params.itemsPerPage;
+        // const constructQuery = {
+        //     name: 'vehicleData',
+        //     text: 'select * from apv_ive.VEHICLE_DATA_REQUEST_INFO limit $1 offset $2 ORDER BY $3 $4',
+        //     values: [body_params.itemsPerPage, offset_de, fieldName, orderBy]
+        // }
+
+        const query1 = itemsPerPage ?
+            `select * from apv_ive.VEHICLE_DATA_REQUEST_INFO  ORDER BY ${fieldName} ${orderBy} limit ${body_params.itemsPerPage} offset ${(body_params.page - 1) * body_params.itemsPerPage}`
+            : `select * from apv_ive.VEHICLE_DATA_REQUEST_INFO ORDER BY ${fieldName} ${orderBy} `
+
+        const getData = params => new Promise((resolve, reject) => {
+            const pool = new pg.Pool(dbConfig);
+            pool.connect(function (err, client, done) {
+                if (err) {
+                    reject(err.stack)
+                    throw Error(err)
+                } else {
+
+                    client.query(query1, (err, result) => {
+                        if (err) {
+                            reject(err.stack)
+                            throw Error(err)
+                        } else {
+                            resolve(result.rows)
+
+                        }
+                        client.release();
+
+
+                    });
+                    pool.end()
+                }
+
+            });
+        })
+        const fetch_data = await getData();
+        response.statusCode = 200;
+        response.body = fetch_data;
+        console.info('Ending lambda at ' + new Date());
+        return response;
+
+    } catch (error) {
+        console.error(error, 'error cought in catch')
+        return error;
+
+    }
 
 };
 
